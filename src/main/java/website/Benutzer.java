3ada9568de962714af39a67d59a2b59c77c7a1b6
ufruehstueck-tgm <31989444+ufruehstueck-tgm@users.noreply.github.com>
2018-05-11 16:47:19 +0200

    added benutzer

---
+++ src/main/java/website/Benutzer.java
@@ -0,0 +1,11 @@
+package website;
+
+public class Benutzer {
+	private int ID;
+	private boolean organisator;
+	private String vname;
+	private String nname;
+	private String email;
+	private String password;
+
+}
