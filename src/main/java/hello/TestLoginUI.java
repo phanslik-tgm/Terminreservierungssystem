73e417b8ade61d9b303edaf392439fb53d99f6e5
Peter Hanslik <peterhanslik1010@gmail.com>
2018-06-07 22:07:35 +0200

    login angefangen

--- src/main/java/hello/TestLoginUI.java
+++ src/main/java/hello/TestLoginUI.java
@@ -1,7 +1,14 @@
 package hello;
 
-/**
- * Created by peter on 07.06.2018.
- */
-public class TestLoginUI {
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import org.springframework.util.StringUtils;
+
+@SpringUI(path="/login")
+public class TestLoginUI
+{
+    
 }
